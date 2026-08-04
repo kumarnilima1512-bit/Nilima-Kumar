6519163d01e7cdf6db0ffe2773f49e1341bc103a
Nilima Kumar <kumarnilima1512@gmail.com>
2026-08-04 11:13:21 +0530

    chore: updated config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss','@nuxtjs/color-mode','@nuxtjs/sitemap'],
   app: {
     head: {
-      viewport: 'width=device-width, initial-scale=1, maximum-scale=5'
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=5',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/images/logo.png' }
+      ]
     }
   },
   colorMode: {
